f98f8b1d27beae60e64b2b253009679bdc96eacf
Vu Toan <vutoan@Vus-MacBook-Pro.local>
2017-01-19 16:28:38 +0700

    active admin enabled

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 # loading particular seed file
 load(Rails.root.join('db', 'seed', "categories.rb")) if ENV["categories"]
+AdminUser.create!(
+  email: 'admin@example.com',
+  password: 'password',
+  password_confirmation: 'password') if ENV["admin"]
